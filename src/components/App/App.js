064bb30b47f3e20fd9b1524ccf5caca1a2f06a57
Iris <irinaais90@gmail.com>
2023-06-15 22:22:18 +0300

    feat: добавляет получение данных из инпутов для дальнейшего поиска пользователя

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,37 @@ import './App.css';
 import SearchForm from '../SearchForm/SearchForm';
 import UserList from '../UserList/UserList';
 
+const usersList = [{
+  email: 'jim@gmail.com',
+  number: '221122',
+}, {
+  email: 'jam@gmail.com',
+  number: '830347',
+}, {
+  email: 'john@gmail.com',
+  number: '221122',
+}, {
+  email: 'jams@gmail.com',
+  number: '349425',
+}, {
+  email: 'jams@gmail.com',
+  number: '141424',
+}, {
+  email: 'jill@gmail.com',
+  number: '822287',
+}, {
+  email: 'jill@gmail.com',
+  number: '822286',
+}];
+
 function App() {
+  function handleSearchUser(email, number) {
+    console.log(email, number);
+  }
+
   return (
     <main className='page'>
-      <SearchForm />
+      <SearchForm onSubmit={ handleSearchUser }/>
       <UserList />
     </main>
   );
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,21 +1,40 @@
 import './SearchForm.css';
+import { useState } from 'react';
 
-function SearchForm() {
+function SearchForm(props) {
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
+
+  function handleChangeEmail(evt) {
+    setEmail(evt.target.value);
+  }
+
+  function handleChangeNumber(evt) {
+    setNumber(evt.target.value);
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    props.onSubmit(email, number);
+  }
+
   return (
     <section className='search-form__container page__section' >
-      <form className='search-form'>
+      <form className='search-form' onSubmit={ handleSubmit }>
         <input
           className='search-form__input'
           type='email'
           placeholder='Почта'
           required
           name='email'
+          onChange={ handleChangeEmail }
         />
         <input
           className='search-form__input'
           type='number'
           placeholder='Номер'
           name='number'
+          onChange={ handleChangeNumber }
           />
         <button className="search-form__button" type="submit" aria-label="Найти пользователя"/>
       </form>
