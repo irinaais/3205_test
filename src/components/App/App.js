b72863052019b3b78c0177f345ebd99d4ab978bb
Iris <irinaais90@gmail.com>
2023-06-19 20:08:57 +0300

    fix: исправляет логику поиска пользователей

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,31 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import UserList from '../UserList/UserList';
 import Preloader from '../Preloader/Preloader';
 import * as Api from '../../utils/Api';
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [number, setNumber] = useState('');
   const [usersFetched, setUsersFetched] = useState(false);
   const [filteredUsersList, setFilteredUsersList] = useState([]);
   const [searchFailed, setSearchFailed] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   function handleSearchUsers(emailFromInput, numberFromInput) {
-    setEmail(emailFromInput);
-    setNumber(numberFromInput);
-    setUsersFetched(true);
+    setIsLoading(true);
+    Api.getUsers(emailFromInput, numberFromInput)
+      .then((res) => setFilteredUsersList(res))
+      .catch((err) => {
+        setSearchFailed(true);
+        console.log(`Ошибка: ${err.status}`);
+      })
+      // Если отключить искусственную задержку на бэке, то раскомментировать эту строку
+      // .finally(() => setTimeout(() => setIsLoading(false), 800));
+      .finally(() => {
+        setIsLoading(false);
+        setUsersFetched(true);
+      });
   }
-
-  useEffect(() => {
-    if (usersFetched) {
-      setIsLoading(true);
-      Api.getUsers(email, number)
-        .then((res) => setFilteredUsersList(res))
-        .catch((err) => {
-          setSearchFailed(true);
-          console.log(`Ошибка: ${err.status}`);
-        })
-        // Если отключить искусственную задержку на бэке, то раскомментировать эту строку
-        // .finally(() => setTimeout(() => setIsLoading(false), 800));
-        .finally(() => setIsLoading(false));
-    }
-  }, [email, number]);
 
   return (
     <main className='page'>
